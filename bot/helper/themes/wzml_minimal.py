06eb90d6b8760a65b918fc874b014c320e075880
Samar khan <166974819+SMDandCo@users.noreply.github.com>
2024-05-09 22:25:08 +0530

    Update wzml_minimal.py

--- bot/helper/themes/wzml_minimal.py
+++ bot/helper/themes/wzml_minimal.py
@@ -173,11 +173,11 @@ class WZMLStyle:
 
     # def get_readable_message(): ---> bot_utilis.py
     ####--------OVERALL MSG HEADER----------
-    STATUS_NAME =       '<b></b>✔️ғɪʟᴇ ɴᴀᴍᴇ :<code>{Name}</code>'
+    STATUS_NAME =       '<b>✔️ғɪʟᴇ ɴᴀᴍᴇ :<code>{Name}</code>\n\n┎━━━━ « <a href="https://t.me/SMD_Botz">𝐒𝐌𝐃 𝐗 𝐂𝐥𝐨𝐮𝐝</a> » ━━━━༻</b>'
 
     #####---------PROGRESSIVE STATUS-------
-    BAR =               '\n┎ </b><a href="https://t.me/SMD_BOTz">{Bar}</a>'
-    PROCESSED =         '\n┠ <b>🛡️ᴘʀᴏᴄᴇssᴇᴅ:</b> {Processed}'
+    BAR =               '\n┠ </b><a href="https://t.me/SMD_BOTz">{Bar}</a>'
+    PROCESSED =         '\n┠ <b>🛡ᴘʀᴏᴄᴇssᴇᴅ:</b> {Processed}'
     STATUS =            '\n┠ <b>🪄sᴛᴀᴛᴜs:</b> <a href="{Url}">{Status}</a>'
     ETA =               '\n┠ <b>🧿ᴇᴛᴀ:</b> {Eta}'
     SPEED =             '\n┠ <b>⏳sᴘᴇᴇᴅ:</b> {Speed}'
@@ -188,11 +188,11 @@ class WZMLStyle:
     LEECHERS =          '\n┠ <b>☘️ʟᴇᴇᴄʜᴇʀs:</b> {Leechers}'
 
     ####--------SEEDING----------
-    SEED_SIZE =      '\n┠ <b>🗃️sɪᴢᴇ: </b>{Size}'
+    SEED_SIZE =      '\n┠ <b>🗃sɪᴢᴇ: </b>{Size}'
     SEED_SPEED =     '\n┠ <b>🧬sᴘᴇᴇᴅ: </b> {Speed}'
     UPLOADED =       '\n┠ <b>🍁ᴜᴘʟᴏᴀᴅᴇᴅ: </b> {Upload}'
-    ʀᴀᴛɪᴏ =           '\n┠ <b>⚓ʀᴀᴛɪᴏ: </b> {Ratio}'
-    ᴛɪᴍᴇ =            '\n┠ <b>❄ᴛɪᴍᴇ: </b> {Time}'
+    ʀᴀᴛɪᴏ =           '\n┠ <b>⚓️ʀᴀᴛɪᴏ: </b> {Ratio}'
+    ᴛɪᴍᴇ =            '\n┠ <b>❄️ᴛɪᴍᴇ: </b> {Time}'
     SEED_ENGINE =    '\n┠ <b>💎ᴇɴɢɪɴᴇ:</b> {Engine}'
 
     ####--------NON-PROGRESSIVE + NON SEEDING----------
@@ -203,7 +203,8 @@ class WZMLStyle:
     USER =              '\n┠ <b>⚙️ᴜsᴇʀ:</b> <code>{User}</code>'
     ID =                '\n┠<b>🫧ɪᴅ:</b> <code>{Id}</code>'
     BTSEL =             '\n┠ <b>💫sᴇʟᴇᴄᴛ:</b> {Btsel}'
-    CANCEL =            '\n┖ {Cancel}\n\n'
+    CANCEL =            '\n┠ {Cancel}\n<b>┖━━━━ « <a href="https://t.me/SMD_Botz">𝐒𝐌𝐃 𝐗 𝐂𝐥𝐨𝐮𝐝</a> » ━━━━༻</b>\n\n'
+
 
     ####------FOOTER--------
     FOOTER = '┎<b><b>♠️ʙᴏᴛ sᴛᴀᴛs</b></b>\n'
@@ -297,3 +298,4 @@ class WZMLStyle:
 ┠<b> ʟᴇᴇᴄʜ sᴜғғɪx :</b> <code>{LSUFFIX}</code>
 ┠<b> ʟᴇᴇᴄʜ ᴅᴜᴍᴘs :</b> <code>{LDUMP}</code>
 ┖<b> ʟᴇᴇᴄʜ ʀᴇᴍɴᴀᴍᴇ :</b> <code>{LREMNAME}</code>'''
+    
